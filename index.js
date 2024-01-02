f116f71bae1474bbfbfbbcc7607b6e824847c58c
om prakash kumar <omjsr29@gmail.com>
2024-01-02 18:16:14 +0530

    drumkit

---
+++ index.js
@@ -0,0 +1,56 @@
+var len= document.querySelectorAll(".drum").length;
+for(var i=0;i<len;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttoninner=this.innerHTML;
+        soundmake(buttoninner);
+        animate(buttoninner);
+    });
+}
+document.addEventListener("keydown",function(event){
+    soundmake(event.key);
+    animate(event.key);
+});
+
+
+function soundmake(key){
+    switch (key){
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        default :console.log(key);
+    }
+
+}
+
+function animate(event){
+    var key=document.querySelector("."+event);
+    key.classList.add("pressed");
+    setTimeout(function(){
+        key.classList.remove("pressed");
+    },100);
+}
